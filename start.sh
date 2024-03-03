e25a283afcae3d230c0b650efb2d8beedcab633c
Wany <sung@wanyne.com>
2024-03-03 03:34:15 +0000

    update actions

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-session=github-actions_cucumbery.com
+session=github-actions_cucumbery-com
 tmux has-session -t $session || tmux new-session -d -s $session
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
