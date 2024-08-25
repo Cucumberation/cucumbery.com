ba5c2224135151655866c907d10f764061ef751d
Wany <sung@wanyne.com>
2024-08-25 08:25:10 +0000

    update workflow

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-session=github-actions_cucumbery-com
+session=ga-cucumberycom
 tmux has-session -t $session || tmux new-session -d -s $session
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
